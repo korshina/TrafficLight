fce768d01b4f9ece1b1127037f66ef112d06df69
anastasiiakorhsina <anastasiiakorshina@gmail.com>
2022-03-01 22:06:05 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Anastasiia Korshina on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
