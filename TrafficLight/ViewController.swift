22f8769def09938eacde25cd53b83a491b9684e7
anastasiiakorhsina <anastasiiakorshina@gmail.com>
2022-03-02 23:01:47 +0300

    Fixes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLigthView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var redLightIsOn = false
+    var yellowLightIsOn = false
+    var greenLightIsOn = false
+    var buttonTappedFirst = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        greenLigthView.layer.cornerRadius = 60
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLigthView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        if buttonTappedFirst {
+            buttonTappedFirst = false
+            startButton.setTitle("NEXT", for: .normal)
+            toggleColorOfLight(colorLightView: redLightView, isOn: redLightIsOn)
+            redLightIsOn = true
+        } else if redLightIsOn {
+            toggleColorOfLight(colorLightView: redLightView, isOn: redLightIsOn)
+            redLightIsOn = false
+            toggleColorOfLight(colorLightView: yellowLightView, isOn: yellowLightIsOn)
+            yellowLightIsOn = true
+        } else if yellowLightIsOn {
+            toggleColorOfLight(colorLightView: yellowLightView, isOn: yellowLightIsOn)
+            yellowLightIsOn = false
+            toggleColorOfLight(colorLightView: greenLigthView, isOn: greenLightIsOn)
+            greenLightIsOn = true
+        } else if greenLightIsOn {
+            toggleColorOfLight(colorLightView: greenLigthView, isOn: greenLightIsOn)
+            greenLightIsOn = false
+            toggleColorOfLight(colorLightView: redLightView, isOn: redLightIsOn)
+            redLightIsOn = true
+        }
+        
+    }
+    
+    func toggleColorOfLight(colorLightView: UIView, isOn: Bool) {
+        colorLightView.alpha = isOn ? 0.3 : 1
+    }
+    
 }
 
